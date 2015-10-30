694c253beac6ffaed3f281f577a530b6a6f63294
verenich <ilyavere@gmail.com>
2015-10-30 19:25:59 +0200

    second try

--- mainCaise.R
+++ mainCaise.R
@@ -13,6 +13,7 @@ library(ROSE)
 # nodes = 2 
 # registerDoSNOW(makeCluster(nodes, type="SOCK"))
 
+#ss
 Bondora = read.csv("http://kodu.ut.ee/~ilyav/data/Bondora.csv",header = TRUE,sep = ",")
 invalid = which(is.na(Bondora$CreditDecision) | is.na(Bondora$IdCancellation)| is.na(Bondora$PostFundingCancellation))
 Bondora = Bondora[-invalid,]
